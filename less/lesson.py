52e1fc739e5c20980dfcb125ef6d82a214b2ffea
Sanqn <sandisvert@gmail.com>
2021-09-07 22:35:43 +0300

    add new function 'delenie'

--- less/lesson.py
+++ less/lesson.py
@@ -6,7 +6,11 @@ def multy(a, b):
 
 def minus(a, b):
     return a - b
+
+def delenie(a, b):
+    return a - b
 if __name__ == '__main__':
     print(plus(2, 5))
     print(multy(2, 5))
     print(minus(2, 5))
+    print(delenie(2, 5))
