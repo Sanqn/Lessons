be5b4f17946c005cfdd8db50464f6c066353e5f6
Sanqn <sandisvert@gmail.com>
2021-09-13 22:30:31 +0300

    find x in tabl multiplus

--- less/lesson.py
+++ less/lesson.py
@@ -2060,6 +2060,12 @@
 #             count += 1
 # print(count)
 
-
+n, x = map(int, input().split())
+count = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 2):
+        if i * j == x:
+            count += 1
+print(count)
 
 
