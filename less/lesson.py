823b42ca8e0b6d723417b6b633c8b53e2f3994bd
Sanqn <sandisvert@gmail.com>
2021-09-13 22:15:08 +0300

    foto and progs

--- less/lesson.py
+++ less/lesson.py
@@ -2044,3 +2044,22 @@
 #     print('No')
 # # print(q)
 
+
+# n, m = map(int, input().split())
+# a = []
+# b = []
+# count = 0
+# for i in range(n):
+#     a.append((list(map(str, input()))))
+# input()
+# for i in range(n):
+#     b.append((list(map(str, input()))))
+# for i in range(n):
+#     for j in range(m):
+#         if a[i][j] == b[i][j]:
+#             count += 1
+# print(count)
+
+
+
+
