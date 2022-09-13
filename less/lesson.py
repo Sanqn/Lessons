87469d292d54a3945eaed19364ff29cfe0f89017
Sanqn <Sandisvert@gmail.com>
2022-09-14 00:36:14 +0300

    numpy

--- less/lesson.py
+++ less/lesson.py
@@ -7835,6 +7835,57 @@ df = pd.DataFrame({'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake
 # df.loc[index, 'age'] += 1
 # print(df)
 
+# df.loc[:, 'age'] += 1
+# print(df)
+# df = pd.DataFrame({'name': ["Alex", "Bob", "Carmen", "Diaz", "Ella", "Forman", "Glen"],
+#                    'age': [20, 27, 35, 55, 18, 21, 35],
+#                    'designation': ["VP", "CEO", "CFO", "VP", "VP", "CEO", "MD"]})
+
+
+# a = df.select_dtypes(include=['float64', 'int64'])
+# for i in a:
+#     print(i, df[i].sum())
+
+# count = 0
+# for i in col:
+#     if i in a:
+#         if int(a[count]) / a[count] == 1:
+#             print(f"{i}: {int(a[count])}")
+#             count += 1
+#         else:
+#             print(f"{i}: {a[count]}")
+#             count += 1
+#
+# print(df.select_dtypes(include=['number']), '=========================')
+# for x in df.describe():
+#     print(x + ":" + str(df[x].sum()))
+
+# group_by = "animal"
+# print(df.groupby(group_by)['age'].mean())
+
+# new_index = "k"
+# new_data = [5.5, "dog", "Belka", "no", 2]
+# del_index = "f"
+# df.loc[new_index] = new_data
+# df = df.drop(del_index)
+# print(df)
+#
+# group_by = "animal"
+# print(df[group_by].value_counts())
+#
+# sort_by = ["age", "visits"]
+# print(df.sort_values(by=sort_by, ascending=[False, True]))
+
+# column = "priority"
+# df[column] = df[column].map({'yes': True, 'no': False, 1: True, 0: False})
+# print(df)
+
+column = 'animal'
+old_value = 'snake'
+new_value = 'python'
+df[column] = df[column].replace(old_value, new_value)
+print(df)
+
 
 # ========================================== requests ===============================================================
 # import requests
